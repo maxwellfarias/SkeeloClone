5226067aa331f9d0d7834ebc054e1f78878bb51e
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-04-30 23:50:37 -0300

    AlamofireAdapter was created

---
+++ Skeelo/Infra/AlamofireAdapter.swift
@@ -0,0 +1,60 @@
+//
+//  AlamofireAdapter.swift
+//  Skeelo
+//
+//  Created by Maxwell Farias on 30/04/24.
+//
+
+import Foundation
+import Alamofire
+
+public class AlamofireAdapter: HttpPostClient {
+    private let session: Session
+    
+    public init(session: Session = .default) {
+        self.session = session
+    }
+    
+    public func fetchBooksBySubject(to url: URL, completion: @escaping (Result<Data?, HttpError>) -> Void) {
+        session.request(url).responseData { dataResponse in
+            guard let statusCode = dataResponse.response?.statusCode else {return completion(.failure(.noConnectivity))}
+            switch dataResponse.result {
+            case .failure: completion(.failure(.noConnectivity))
+            case .success(let data):
+                switch statusCode {
+                case 204:
+                    completion(.success(nil))
+                case 200...299:
+                    completion(.success(data))
+                case 401:
+                    completion(.failure(.unauthorized))
+                case 403:
+                    completion(.failure(.forbidden))
+                case 400...499:
+                    completion(.failure(.badRequest))
+                case 500...599:
+                    completion(.failure(.serverError))
+                default: completion(.failure(.noConnectivity))
+                }
+            }
+        }
+    }
+}
+
+public enum HttpError: Error {
+    case noConnectivity
+    case badRequest
+    case serverError
+    case unauthorized
+    case forbidden
+}
+
+public protocol HttpPostClient {
+    func fetchBooksBySubject(to url: URL, completion: @escaping (Result<Data?, HttpError>) -> Void)
+}
+
+public extension Data {
+    func toModel<T:Decodable>() -> T? {
+        return try? JSONDecoder().decode(T.self, from: self)
+    }
+}
