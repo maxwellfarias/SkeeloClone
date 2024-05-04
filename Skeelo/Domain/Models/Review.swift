5ba75bbef8238073b18d58b8131acf7ca94e343c
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-05-04 03:39:08 -0300

    Detail book list was udapte

---
+++ Skeelo/Domain/Models/Review.swift
@@ -0,0 +1,8 @@
+//
+//  Review.swift
+//  Skeelo
+//
+//  Created by Maxwell Farias on 04/05/24.
+//
+
+import Foundation
---
+++ Skeelo/Presentation/DetailBookView/View/ReviewsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewsView.swift
+//  Skeelo
+//
+//  Created by Maxwell Farias on 04/05/24.
+//
+
+import SwiftUI
+
+struct ReviewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewsView()
+}
